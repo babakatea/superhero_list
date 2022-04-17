540f307d865e653462ff8d58bcbd172c55fe3f81
Ecaterina Baba <e.baba@s7.ru>
2022-04-17 19:57:58 +0300

    Modify project structure, implement routing

---
+++ frontend/src/services/login.ts
@@ -0,0 +1,14 @@
+import api from "../configs/api";
+
+export const login = async () => {
+    const authData =  { name: "Test", password: "1234" };
+
+    try {
+        await api.post('/login', authData);
+        localStorage.setItem('token', 'token');
+
+        return Promise.resolve();
+    } catch (e: any) {
+        throw new Error(e.response.data.message);
+    }
+}
